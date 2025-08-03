07b8b74f985aa0efd9fc33b49d3685d0b2178486
B M KIRAN <bmkiran79@gmail.com>
2025-08-03 21:00:31 +0530

    Add Add Income button and modal, ensure all test cases pass

--- src/components/AppHead/AppHead.jsx
+++ src/components/AppHead/AppHead.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 //styles
 import "./AppHead.css"
 
 //components
 import Card from '../Card/Card';
 import PieChartComp from '../PieChart/PieChart';
+import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
 
 const AppHead = props => {
     //props
     const { balance, expenses } = props;
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <header className='AppHead'>
             <Card text="Wallet balance" value={balance}/>
             <Card text="Expenses" value={expenses}/>
             <PieChartComp />
+            <Button
+                text="Add Income"
+                background="primary"
+                buttonSize="large"
+                clickFunction={() => setShowModal(true)}
+            />
+            {showModal && (
+                <Modal
+                    toggleModal={() => setShowModal(false)}
+                    text="Add Balance"
+                />
+            )}
         </header>
     );
 };
